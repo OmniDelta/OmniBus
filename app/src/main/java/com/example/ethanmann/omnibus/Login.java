6e45af6b14d6d9700c974b541d8792b9745e0e97
mcfuzzyman <mcfuzzyman@users.noreply.github.com>
2017-11-12 21:32:59 -0500

    This is my thing

--- app/src/main/java/com/example/ethanmann/omnibus/Login.java
+++ app/src/main/java/com/example/ethanmann/omnibus/Login.java
@@ -6,11 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
 public class Login extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
     }
     public void login(View view)
     {
