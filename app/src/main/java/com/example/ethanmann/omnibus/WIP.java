ff85d539470c66d89b84fe52d759d214f0b9c3d7
OmniDelta <33429888+OmniDelta@users.noreply.github.com>
2017-12-08 14:17:51 -0500

    added WIP page for pages that are under construction or are not being worked on yet.

---
+++ app/src/main/java/com/example/ethanmann/omnibus/WIP.java
@@ -0,0 +1,12 @@
+package com.example.ethanmann.omnibus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class WIP extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wip);
+    }
+}
